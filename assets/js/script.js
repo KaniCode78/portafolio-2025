fa1ad1b6365058eec82a7f96f9bc512ab03eabad
Kaniltazo <angeldanielrm@icloud.com>
2025-01-14 21:07:44 -0500

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,20 @@ window.onscroll = function (){
         }
     }
 }
+
+// Navbar link
+
+const navbar = document.querySelector(".navbar");
+a = navbar.querySelectorAll("a");
+
+a.forEach(function (element) {
+    element.addEventListener("click", function () {
+    for (let i = 0; i < a.length; i++){
+        a[i].classList.remove("active");
+        
+    }
+
+            this.classList.add("active");
+            document.querySelector(".navbar").classList.toggle("show");
+        })
+})
